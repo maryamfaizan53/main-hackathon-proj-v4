c06b2a012b6cf7875c7ad2c2d0faa65f67bf18d9
maryamfaizan53 <maryam.faizanmianoor@gmail.com>
2025-01-22 21:17:52 +0500

    ,,

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextResponse } from "next/server";
-import { sanityClient } from "../../../sanity/lib/client";
-import { allBlogsQuery } from "../../../sanity/queries";
+import { sanityClient } from "../../sanity/lib/client"
+
+import { allBlogsQuery } from "../../sanity/queries";
 
 export async function GET() {
   try {
